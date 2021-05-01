fee933b519fbac78706d79aceffa5e9ac45bcd02
amitksms97 <amitksms97@gmail.com>
2021-05-01 12:02:07 +0530

    Add project files.

---
+++ LearningList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> customer = new List<string>();
+            customer.Add("Kim");
+            customer.Add("John");
+            customer.Add("Tim");
+
+            Console.WriteLine(customer.Count);
+
+            foreach (var customers in customer)
+            {
+                Console.WriteLine(customers);
+            }
+
+            Console.WriteLine(customer[1]);
+        }
+    }
+}
